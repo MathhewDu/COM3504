e7f15c007a91e359af9e36954a16a72f5b4fad72
Tian Yi <160076z@mymail.nyp.edu.sg>
2021-03-19 15:09:18 +0800

    add socket.io

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
 let name = null;
 let roomNo = null;
-let socket=null;
+let socket = io();
 
 
 /**
@@ -14,6 +14,26 @@ function init() {
     document.getElementById('chat_interface').style.display = 'none';
 
     //@todo here is where you should initialise the socket operations as described in teh lectures (room joining, chat message receipt etc.)
+    initSocket();
+}
+
+function initSocket(){
+    // called when someone joins the room. If it is someone else it notifies the joining of the room
+    socket.on('joined', function (room, userId){
+        if (userId == name){
+            //it enters the chat
+            hideLoginInterface(room, userId);
+        } else {
+            //notifies that someone has joined the room
+            writeOnHistory('<b>' + userId + '</b>' + 'joined room ' + room);
+        }
+    });
+    //called when a message is received
+    socket.on('chat', function (room, userId, chatText){
+        let who = userId
+        if(userId == name) who = 'Me';
+        writeOnHistory('<b>' + who + ':</b> ' + chatText);
+    });
 }
 
 /**
@@ -33,6 +53,7 @@ function generateRoom() {
 function sendChatText() {
     let chatText = document.getElementById('chat_input').value;
     // @todo send the chat message
+    socket.emit('chat', roomNo, name, chatText);
 }
 
 /**
@@ -45,6 +66,7 @@ function connectToRoom() {
     let imageUrl= document.getElementById('image_url').value;
     if (!name) name = 'Unknown-' + Math.random();
     //@todo join the room
+    socket.emit('create or join', roomNo, name);
     initCanvas(socket, imageUrl);
     hideLoginInterface(roomNo, name);
 }
--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -2,7 +2,20 @@
 exports.init = function(io) {
   io.sockets.on('connection', function (socket) {
     try {
-     // insert here your event
+      // insert here your event
+      /**
+       * create or joins a room
+       */
+      socket.on('create or join', function(room, userId){
+        socket.join(room);
+        io.sockets.to(room).emit('joined', room, userId);
+      });
+      socket.on('chat', function (room, userId, chatText){
+        io.sockets.to(room).emit('chat', room, userId, chatText);
+      });
+      socket.on('disconnect', function(){
+        console.log('someone disconnected');
+      });
     } catch (e) {
     }
   });
