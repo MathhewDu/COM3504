5661146a92f54d5c9e8d4813728e4f827a01b665
Tian Yi <160076z@mymail.nyp.edu.sg>
2021-03-23 23:48:13 +0800

    register service worker

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+var dataCacheName = 'chatroom-cache-v1';
+var cacheName = 'chatroom-step-8-1';
+var filesToCache = [
+    '/',
+    '/scripts/app.js',
+    '/stylesheets/style.css',
+    '/javascripts/idb/index.js',
+    '/javascripts/idb/wrap-idb-value.js',
+    '/javascripts/canvas.js',
+    '/javascripts/database.js',
+    '/javascripts/index.js'
+];
