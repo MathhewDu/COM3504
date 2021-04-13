fb855ba3f564a8fdbecccd0428e3dc11fd872607
Yuyang <qq18261434655@gmail.com>
2021-04-13 22:02:48 +0800

    new way upload image to server

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var tools =require("../models/tools")
+
+var multer = require('multer')
+const path = require('path')
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb){
+    cb(null, '../public/images')
+  },
+  filename: function(req, file, cb){
+    let extname = path.extname(file.originalname)
+    cb(null,Date.now()+ extname)
+
+  }
+})
+var upload = multer({storage: storage })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,6 +31,14 @@ router.post('/', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(imgData));
   }
+});
+router.post('/doAdd', tools.multer().single("pic"), function(req,res, next) {
+  res.render('index', { title: 'Image Browsing', success:'haaa'});
+  // res.send("aaaa")
+  res.send({
+    body:req.body,
+    file:req.file
+  });
 });
 
 
