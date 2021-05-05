7b9b4bc355c159ae8f94b3f61043dc58dfd21652
Tian Yi <160076z@mymail.nyp.edu.sg>
2021-05-05 17:32:46 +0800

    add knowledge graph

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,8 @@
 let name = null;
 let roomNo = null;
 let socket = io();
+const service_url = 'https://kgsearch.googleapis.com/v1/entities:search';
+const apiKey= 'AIzaSyAG7w627q-djB4gTTahssufwNOImRqdYKM';
 /**
  * called by <body onload>
  * it initialises the interface and the expected socket messages
@@ -20,6 +22,31 @@ function init() {
     //         .then(function() { console.log('Service Worker Registered'); });
     // }
     // loadData(false);
+
+    let type= document.getElementById("myType").value;
+
+    let config = {
+        'limit': 10,
+        'languages': ['en'],
+        'types': [type],
+        'maxDescChars': 100,
+        'selectHandler': selectItem,
+    }
+    KGSearchWidget(apiKey, document.getElementById("myInput"), config);
+    //document.getElementById('widget').style.display='block';
+
+}
+
+function selectItem(event){
+    let row= event.row;
+    //document.getElementById('resultImage').src= row.json.image.url;
+    document.getElementById('resultId').innerText= 'id: '+row.id;
+    document.getElementById('resultName').innerText= row.name;
+    document.getElementById('resultDescription').innerText= row.rc;
+    document.getElementById("resultUrl").href= row.qc;
+    document.getElementById('resultPanel').style.display= 'block';
+    //document.getElementById('resultPanel').innerHTML= '<PRE>'+JSON.stringify(row, null, 4)+'</PRE>';
+
 }
 
 function initSocket(){
