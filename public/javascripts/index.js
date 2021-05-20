2e0abe28f10a119850cd17a171addb8b212a8646
Tian Yi <160076z@mymail.nyp.edu.sg>
2021-05-20 19:08:54 +0800

    update knowledge graph

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -24,17 +24,32 @@ function init() {
     }
     loadData(false);
 
-    let type= document.getElementById("myType").value;
 
-    let config = {
-        'limit': 10,
-        'languages': ['en'],
-        'types': [type],
-        'maxDescChars': 100,
-        'selectHandler': selectItem,
+}
+
+function widgetInit(){
+    let type= document.getElementById("myType").value;
+    if(type){
+        let config = {
+            'limit': 10,
+            'languages': ['en'],
+            'types': [type],
+            'maxDescChars': 100,
+            'selectHandler': selectItem,
+        }
+
+        KGSearchWidget(apiKey, document.getElementById("myInput"), config);
+        document.getElementById('typeSet').innerHTML= 'of type: '+type;
+        document.getElementById('widget').style.display='block';
+        document.getElementById('typeForm').style.display= 'none';
+    }else {
+        alert('Set the type please');
+        document.getElementById('widget').style.display='none';
+        document.getElementById('resultPanel').style.display='none';
+        document.getElementById('typeSet').innerHTML= '';
+        document.getElementById('typeForm').style.display= 'block';
     }
-    KGSearchWidget(apiKey, document.getElementById("myInput"), config);
-    //document.getElementById('widget').style.display='block';
+
 
 }
 
